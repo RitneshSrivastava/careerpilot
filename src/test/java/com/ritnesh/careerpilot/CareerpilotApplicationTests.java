b7e32eaf9ee3c67583865922df6e1c3567c60637
RitneshSrivastava <ritneshntv@gmail.com>
2026-07-09 18:33:46 +0530

    Initialize Spring Boot project structure

---
+++ src/test/java/com/ritnesh/careerpilot/CareerpilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ritnesh.careerpilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerpilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
