e17119480171ee422a41caac77959f8e5316c530
RitneshSrivastava <ritneshntv@gmail.com>
2026-07-22 13:32:45 +0530

    Complete JWT authentication module with Spring Security

--- src/main/java/com/ritnesh/careerpilot/util/JwtUtil.java
+++ src/main/java/com/ritnesh/careerpilot/util/JwtUtil.java
@@ -1,27 +1,61 @@
 package com.ritnesh.careerpilot.util;
+
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
-
 import java.util.Date;
+import java.util.function.Function;
 
 @Component
 public class JwtUtil {
+
     private static final String SECRET_KEY =
             "careerpilotbackendprojectjwtsecretkey2026secure";
 
     private static final long EXPIRATION_TIME = 1000 * 60 * 60;
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
     public String generateToken(String email) {
-
-        SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
-
         return Jwts.builder()
                 .subject(email)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(key)
+                .signWith(getSigningKey())
                 .compact();
+    }
+
+    public String extractEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public boolean isTokenValid(String token, String email) {
+        return extractEmail(token).equals(email) && !isTokenExpired(token);
     }
 }
