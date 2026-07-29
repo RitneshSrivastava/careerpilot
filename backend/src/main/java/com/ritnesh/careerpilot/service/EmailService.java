382a8f921899baa64172d5017c165beeca05cb54
RitneshSrivastava <ritneshntv@gmail.com>
2026-07-29 16:09:54 +0530

    Added my name in the OTP verification

--- backend/src/main/java/com/ritnesh/careerpilot/service/EmailService.java
+++ backend/src/main/java/com/ritnesh/careerpilot/service/EmailService.java
@@ -45,7 +45,9 @@ public class EmailService {
                 "textContent",
                 "Your CareerPilot verification code is: " + otpCode +
                         "\n\nThis code expires in 10 minutes.\n\n" +
-                        "If you didn't request this, you can ignore this email."
+                        "If you didn't request this, you can ignore this email." +
+                        "Best Regards.\n\n" +
+                        "Ritnesh Srivastav.\n\n"
         );
 
         HttpEntity<Map<String, Object>> request = new HttpEntity<>(body, headers);
